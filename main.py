4cce7f6fbec9129c58f3a05330f3d45cf79418c7
vapelavsky <mscn.brtlmo@gmail.com>
2021-07-20 16:55:29 +0300

    Created class Human and tested it

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 # Test Task for Intersog
 
-if __name__ == "__main__":
-    print("Hello world!")
+class Human:
+
+    """This is the human from TikTok and she can to entertain you"""
+
+    def __init__(self):
+        print("Hello, my name is Zuzie, that's Zuzie with a Z! O_O")
+
+    def __call__(self, username):
+        print(f"Hello, my name is Zuzie! Nice to meet you, {username}")
+
+    def __str__(self, username, friendname):
+        return f'My name is Zuzie, your name is {username}, name of your friend is {friendname}'
+
+h = Human()
+h("Kyrylo")
+h.__str__("Kyrylo", "Vladyslav")
+print(h.__doc__)
