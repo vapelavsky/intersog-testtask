8d8f88a3a513add6936f80e94aa01480f8cbdc70
vapelavsky <mscn.brtlmo@gmail.com>
2021-07-20 18:08:43 +0300

    Added attributes age, sex, hobbies, country; Added methods drink, travel, calculate

--- main.py
+++ main.py
@@ -2,18 +2,37 @@
 
 class Human:
 
-    """This is the human from TikTok and she can to entertain you"""
+    """This is the human from TikTok and she can entertain you.
+    Available methods:
+    1. drink
+    2. travel
+    3. sum calculate
+    4. """
+    age = 28
+    sex = "F"
+    hobbies = ["Music", "Cinema", "Active leisure"]
+    country = "Ukraine"
 
-    def __init__(self):
-        print("Hello, my name is Zuzie, that's Zuzie with a Z! O_O")
+    @classmethod
+    def __init__(cls):
+        print(f"Hello, my name is Zuzie, that's Zuzie with a Z! I'm {cls.age} years old")
 
     def __call__(self, username):
-        print(f"Hello, my name is Zuzie! Nice to meet you, {username}")
+        return f'Hello, my name is Zuzie! Nice to meet you, {username}'
 
     def __str__(self, username, friendname):
         return f'My name is Zuzie, your name is {username}, name of your friend is {friendname}'
 
-h = Human()
-h("Kyrylo")
-h.__str__("Kyrylo", "Vladyslav")
-print(h.__doc__)
+    @staticmethod
+    def drink(name):
+        return f'So, I drunk {name}'
+
+    @classmethod
+    def travel(cls, place):
+        return f'I travelled from {cls.country} to the {place}'
+
+    @staticmethod
+    def calculate(a, b):
+        return f"I'm very smart, so I can calculate sum of given numbers.\n" \
+               f"Given numbers is {a} and {b}.\n" \
+               f"So, your answer is {a + b} \n"
