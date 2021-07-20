28bc27e422106124ad8b753db1db0dc65fc0bc85
vapelavsky <mscn.brtlmo@gmail.com>
2021-07-20 19:08:45 +0300

    Finally, it works

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # Test Task for Intersog
 
 class Human:
-
     """This is the human from TikTok and she can entertain you.
     Available methods:
     1. drink
@@ -9,17 +8,74 @@ class Human:
     3. sum calculate
     4. sleep
     5. show hobbies"""
-    age = 28
-    sex = "F"
-    hobbies = ["Music", "Cinema", "Active leisure"]
-    country = "Ukraine"
-    job = "TikTok Bloger"
-    hair = "Blue"
-    height = 168
+    name: str
+    age: int
+    sex: str
+    hobbies: list
+    country: str
+    job: str
+    hair: str
+    height: int
 
-    @classmethod
-    def __init__(cls):
-        print(f"Hello, my name is Zuzie, that's Zuzie with a Z! I'm {cls.age} years old")
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, name: str):
+        self.__name = name
+
+    def get_age(self):
+        return self.__age
+
+    def set_age(self, age: int):
+        self.age = age
+
+    def get_sex(self):
+        return self.__sex
+
+    def set_sex(self, sex: str):
+        self.__sex = sex
+
+    def get_height(self):
+        return self.__height
+
+    def set_height(self, height: float):
+        self.__height = height
+
+    def get_hair(self):
+        return self.__hair
+
+    def set_hair(self, hair: str):
+        self.__hair = hair
+
+    def get_job(self):
+        return self.__job
+
+    def set_job(self, job: str):
+        self.__job = job
+
+    def get_hobbies(self, hobbies: list):
+        return self.__hobbies
+
+    def set_hobbies(self, hobbies: list):
+        self.__hobbies = hobbies
+
+    def __init__(self, name, age, sex, hobbies, country, job, hair, height):
+        self.__name = name
+        self.__age = age
+        self.__sex = sex
+        self.__hobbies = hobbies
+        self.__country = country
+        self.__job = job
+        self.__hair = hair
+        self.__height = height
+        print(f"Hello! My name is {self.__name} with {self.__hair} hair. \n"
+              f"I'm {self.__age} years old and my height is {self.__height}. \n"
+              f"My sex is {self.__sex}. \n"
+              f"I'm from {self.__country}. \n"
+              f"I'm working as {self.__job}")
+        print("My hobbies is")
+        for hobby in self.__hobbies:
+            print(hobby)
 
     def __call__(self, username):
         return f'Hello, my name is Zuzie! Nice to meet you, {username}'
@@ -31,9 +87,8 @@ class Human:
     def drink(name):
         return f'So, I drunk {name}'
 
-    @classmethod
-    def travel(cls, place):
-        return f'I travelled from {cls.country} to the {place}'
+    def travel(self, place):
+        return f'I travelled from {self.__country} to the {place}'
 
     @staticmethod
     def calculate(a, b):
@@ -45,10 +100,54 @@ class Human:
     def sleep(hours):
         return f"Ok, dear, I will sleep for {hours} hours"
 
-    @classmethod
-    def showhobbies(cls):
+    def showhobbies(self):
         print("My hobbies is")
-        for hobby in cls.hobbies:
+        for hobby in self.__hobbies:
             print(hobby)
 
         return "Great, that's all"
+
+
+class Child(Human):
+    def __init__(self, name, age, sex, hobbies, country, job, hair, height):
+        if age < 16:
+            job = "Unemployed"
+        super(Child, self).__init__(name, age, sex, hobbies, country, job, hair, height)
+
+    def set_job(self, job: str):
+        if self.__age < 16:
+            self.__job = "Unemployed"
+
+    def walk(self):
+        print(f"{self.get_name()} walked successfully")
+
+    def work(self):
+        if self.get_job() == "Unemployed":
+            print(f"I'm child, come on.")
+
+
+if __name__ == "__main__":
+    human = Human(name="Zuzie",
+                  age=26,
+                  sex="Female",
+                  hobbies=["Music", "Active leisure", "TikTok"],
+                  country="Ukraine",
+                  job="Database crasher on production",
+                  hair="Blue",
+                  height=180)
+    print(human.drink("Cola"))
+    print(human.travel("Switzerland"))
+    print(human.calculate(12, 14))
+    print(human.sleep(12))
+    print(human.showhobbies())
+
+    child = Child(name="Zizzie",
+                  age=13,
+                  sex="Male",
+                  hobbies=["TikTok", "Active leisure", "Computers"],
+                  country="Ukraine",
+                  job="Database crasher on production",
+                  hair="Yellow",
+                  height=156)
+    child.walk()
+    child.work()
